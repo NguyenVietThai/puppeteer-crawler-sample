8a8ab402e0c21902831a757adaba959873dbb07f
prNguyenVietThai <thai.nguyenviet@prtimes.co.jp>
2022-06-22 14:46:32 +0900

    puppeteer-crawler-sample

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # puppeteer-crawler-sample
 Puppeteerで開発したクローラ
+
+```sh
+puppeteer-crawler-sample
+    |
+    |---crawler
+    |    |---phpCrawler.php -> PHP-curlクローラ
+    |    |---puppCrawler.js -> Puppeteerクローラ
+    |    |...
+    |
+    |---frontend -> ReactJSプロジェクト
+    |    |---public
+    |    |    |---index.html -> HTMLファイル
+    |    |    |---...
+    |    |
+    |    |---src
+    |    |    |---App.tsx -> 記事リストを含むコンポーネント
+    |    |    |---...
+    |    |
+    |    |...  
+    |      
+    |---README.md
+```
+
+# How to run
+
+### Frontend
+```sh
+cd frontend
+npm ci
+npm start
+```
+
+### Crawler
+```sh
+cd crawler
+npm ci
+
+# Puppeteer crawler
+npm run pupp-crawler
+
+# PHP-curl crawler
+npm run php-crawler
+```
---
+++ crawler/phpCrawler.php
@@ -0,0 +1,28 @@
+<?php
+
+$url = 'http://localhost:3000/';
+$curl = curl_init();
+// オプションのセット
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HEADER, 1);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION,true);//リダイレクトをたどる
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//証明書の検証を行わない
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);//curl_execの結果を文字列で返す
+curl_setopt($curl, CURLOPT_ENCODING, "gzip");
+
+// 実行
+$res1 = curl_exec($curl);
+$res2 = curl_getinfo($curl);
+
+if($res2["http_code"] !== 200) {
+    var_dump("Failed to craw page with status ".$res2["http_code"]);// 終了
+    curl_close($curl);
+    exit();
+}
+
+// 終了
+curl_close($curl);
+
+$html = substr($res1, $res2['header_size']);    // 取得したデータ(JSONなど)
+
+var_dump($html);
